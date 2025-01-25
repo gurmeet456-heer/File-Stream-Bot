4ca3414dff51e381ce84c7054b7c1030bdd4e413
gurmeet456-heer <gurmeet20042004@gmail.com>
2025-01-25 08:47:26 -0500

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -15,22 +15,22 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
+    API_ID = int(getenv('API_ID', '27167684'))
+    API_HASH = str(getenv('API_HASH', '9900d75ba4720fe29771c52e468d3cf3'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7094932693:AAE5k9hXsNv5YXL7JvBUw1KWb22wnRpHMQI'))
     PICS = (environ.get('PICS', 'https://envs.sh/jUp.jpg')).split()
-    name = str(getenv('name', 'linkstreamrobot'))
+    name = str(getenv('name', 'Maketmovie_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1005329804168'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002210035725'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5329804168").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'TechifyRahul'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Gurmeet01k'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -43,11 +43,11 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TechifyBots')) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://gurmeet1919:1gurmeet@cluster0.chjdcci.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'moviemarket_official')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @CallOwnerBot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
     SHORTLINK = is_enabled('SHORTLINK', False)
-    SHORTLINK_URL = getenv('SHORTLINK_URL', '')
-    SHORTLINK_API = getenv('SHORTLINK_API', '')
+    SHORTLINK_URL = getenv('SHORTLINK_URL', 'mdiskshortner.link')
+    SHORTLINK_API = getenv('SHORTLINK_API', 'd164c57265f68e1d934e3aa69b204ee179efa6d4')
